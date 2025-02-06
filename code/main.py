9715409b3e52c98704c008d82b7d70a0bcd29ff6
Firdevs Çolak <firdevscolak81@gmail.com>
2025-02-06 17:01:42 +0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -19,19 +19,22 @@ snail_rect = snail_surf.get_rect(midbottom= (500,280))
 player_surf= pygame.image.load('images\katana kedy.png').convert_alpha()
 player_rect= player_surf.get_rect(midbottom= (40,281))
 
-text_surf= font.render('my game',True,'Black')
+score_surf= font.render('my game',True,'Black')
+score_rect= score_surf.get_rect(center= (400,50)) 
 
 while True: 
-    for event in pygame.event.get(): #looking for inputs
+    for event in pygame.event.get(): #looking for inputs, event for real time actions 
         if event.type == pygame.QUIT: 
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEMOTION: #down and up: clicking or not 
-            
+        if event.type == pygame.MOUSEMOTION: #down and up: clicking or not  
+            if player_rect.collidepoint(event.pos): print('lalal')           
 
     screen.blit(background_surf,(0,0)) 
     screen.blit(ground_surf,(0,280)) #block image transfer// putting to surface 
-    screen.blit(text_surf,(300,50))
+    #pygame.draw.rect(screen,'black',score_rect)
+    #pygame.draw.line(screen,'gold',(0,0),(800,400),10)
+    screen.blit(score_surf,score_rect)
 
     snail_rect.x -=4
     if snail_rect.right <=0: snail_rect.left = 800
@@ -41,10 +44,9 @@ while True:
     if player_rect.colliderect(snail_rect): #collision check
         print('ded')
     
-    mouse_pos= pygame.mouse.get_pos() #getting mouse pos mouse pressed-> (left,mid,right)
-    if player_rect.collidepoint(mouse_pos):
-        #pygame.mouse
-    
+    #mouse_pos= pygame.mouse.get_pos() #getting mouse pos mouse pressed-> (left,mid,right)
+    #if player_rect.collidepoint(mouse_pos):
+    #    print(pygame.mouse.get_pressed())
     
     pygame.display.update()
     clock.tick(60) 
